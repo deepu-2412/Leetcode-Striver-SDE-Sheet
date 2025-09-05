8d4ee053e8842339a9038cdcacf888aa219e56d7
deepu-2412 <142926240+deepu-2412@users.noreply.github.com>
2025-09-05 23:07:55 +0530

    Delete Twosum.py

--- Twosum.py
+++
@@ -1,8 +0,0 @@
-def twoSum(self, nums: List[int], target: int) -> List[int]:
-  o = []
-  for i in range(0,len(nums)):
-    for j in range(i+1,len(nums)):
-      if nums[i]+nums[j] == target:
-        o.append(i)
-        o.append(j)
-        return(o)
